9421f8dd4f87f60533ca2ab7088da01f7344b7c7
Ivan Vera <ivandejesusvera@gmail.com>
2025-01-02 19:18:42 -0500

    Actividad en clase, logica para calcular

--- app/src/main/java/com/example/clase_android_ivan/MainActivity.kt
+++ app/src/main/java/com/example/clase_android_ivan/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.clase_android_ivan
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //variables globales
+    /*
+    modificadordeacceso
+    * */
+    private lateinit var numeroUsuario:EditText
+    private lateinit var calcular:Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +28,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        numeroUsuario=findViewById(R.id.numeroUsuario)
+        calcular=findViewById(R.id.btnCalcular)
+        calcular.setOnClickListener {
+            calcularEdad()
+        }
+    }
+    fun calcularEdad(){
+        val edad = numeroUsuario.text.toString()
+        //logica para calcular
+        if(edad > 18.toString()){
+            Toast.makeText(this, "Usted es mayor de edad",Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(this, "Usted es menor de edad",Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
